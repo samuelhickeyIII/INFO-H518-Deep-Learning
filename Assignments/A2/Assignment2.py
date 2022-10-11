97ff243001885cac9fd41b7b6070ab3ce63dd62d
Sam Hickey <52939970+samuelhickeyIII@users.noreply.github.com>
2022-10-11 17:23:56 -0400

    Merge

--- Assignments/A2/Assignment2.py
+++ Assignments/A2/Assignment2.py
@@ -8,6 +8,7 @@ DATA: handwritten digits classified into two classes Even (0) and Odd (1)
 """
 
 
+from operator import le
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -27,7 +28,6 @@ test_set_x_orig = []
 test_set_y = []
 classes = ['even','odd']
 
-
 data_size = len(images_and_labels)
 # Setting the testset size
 test_set_size = 200
@@ -38,14 +38,14 @@ test_set_size = 200
 
 for i in range(len(images_and_labels))[:-test_set_size]:
     train_set_x_orig.append(images_and_labels[i][0])
-    if images_and_labels[i][1] %2 == 0: ##if even put 0 as the label
+    if images_and_labels[i][1] % 2 == 0: ##if even put 0 as the label
         train_set_y.append(0)
     else: ##if odd put 1 as the label
         train_set_y.append(1)
         
 for i in range(len(images_and_labels))[-test_set_size:]:
     test_set_x_orig.append(images_and_labels[i][0])
-    if images_and_labels[i][1] %2 == 0: ##if even put 0 as the label
+    if images_and_labels[i][1] % 2 == 0: ##if even put 0 as the label
         test_set_y.append(0)
     else: ##if odd put 1 as the label
         test_set_y.append(1)
@@ -65,7 +65,11 @@ print(
 # Display an example from the data
 index = 1
 plt.imshow(train_set_x_orig[index])
-print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' number.")
+# plt.show()
+print (
+    f'y = {str(train_set_y[:, index])}, it is a '
+    f'"{classes[np.squeeze(train_set_y[:, index])]}" number.'
+)
 
 m_train = train_set_y.shape[1]
 m_test = test_set_y.shape[1]
@@ -97,7 +101,7 @@ train_set_x = train_set_x_flatten/255.
 test_set_x = test_set_x_flatten/255.
 
 
-###COMPLETE THE FUNCTION
+###COMPLETE THE FUNCTION: Completed
 def sigmoid(z):
     """
     Compute the sigmoid of z
@@ -108,12 +112,12 @@ def sigmoid(z):
     Return:
     s -- sigmoid(z)
     """
-    s = 1.0 / (1.0 + np.exp(-result))
+    s = 1.0 / (1.0 + np.exp(-z))
     
     return s
 
 
-###COMPLETE THE FUNCTION
+###COMPLETE THE FUNCTION: Completed
 def initialize_with_zeros(dim):
     """
     This function creates a vector of zeros of shape (dim, 1) for w and
@@ -126,22 +130,18 @@ def initialize_with_zeros(dim):
     w -- initialized vector of shape (dim, 1)
     b -- initialized scalar (corresponds to the bias)
     """
-    
+    w = np.zeros((dim, 1))
+    b = 0
     
     ## veryifying the shape of the w vector
     assert(w.shape == (dim, 1))
     assert(isinstance(b, float) or isinstance(b, int))
-
-
     
     return w, b
 
 
-
-
-
 ###COMPLETE THE FUNCTION
-def propagate(w, b, X, Y):
+def propagate(w: np.array, b: float, X: np.array, Y: np.array):
     """
     Implement the cost function and its gradient for the propagation 
     explained above
@@ -157,30 +157,22 @@ def propagate(w, b, X, Y):
     cost -- negative log-likelihood cost for logistic regression
     dw -- gradient of the loss with respect to w, thus same shape as w
     db -- gradient of the loss with respect to b, thus same shape as b
-    
-    
     """
     
     m = float(X.shape[1])
     
     # FORWARD PROPAGATION (FROM X TO COST)
-    A =          # put the activation computation code here
-    cost =        # put the cost computation code here
+    A = sigmoid(np.dot(w.reshape((1, 64)), X) + b)
+    cost = -(1/m) * np.sum(Y * np.log(A) + (1-Y) * np.log(1-A))
 
     # BACKWARD PROPAGATION (TO FIND GRADIENTS)
-    
-    dw = # put the code to compute dw
-    db =  # put the code to compute db
-    
-    cost = np.squeeze(cost)
-    
+    dw = np.dot((A-Y), X.T)
+    db = np.sum(A - Y)
+
     grads = {"dw": dw,
              "db": db}
     
     return grads, cost
-
-
-
 
 
 ###COMPLETE THE FUNCTION
@@ -192,34 +184,30 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
     w -- weights, a numpy array of size (num_px * num_px, 1)
     b -- bias, a scalar
     X -- data of shape (num_px * num_px, number of examples)
-    Y -- true "label" vector (containing 0 if even, 1 if odd), of shape (1, number of examples)
+    Y -- true "label" vector (containing 0 if even, 1 if odd), of shape 
+        (1, number of examples)
     num_iterations -- number of iterations of the optimization loop
     learning_rate -- learning rate of the gradient descent update rule
     print_cost -- True to print the loss every 100 steps
     
     Returns:
     params -- dictionary containing the weights w and bias b
-    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
-    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
-    
-    
+    grads -- dictionary containing the gradients of the weights and bias with
+        respect to the cost function
+    costs -- list of all the costs computed during the optimization, this will
+        be used to plot the learning curve.
     """
-    
     costs = []
-    
     for i in range(num_iterations):
-        
-                 
-        grads, cost = # call the function that does this part
-        
-        
+
+        grads, cost = propagate(w=w, b=b, X=X, Y=Y)
         
         dw = grads["dw"]
         db = grads["db"]
         
-        w = # update w using dw and learning rate
-        b = # update b using db and learning rate
-        
+        w = np.array([w[i] - learning_rate * dw[i] for i in range(dw.shape[0])])
+        b = -learning_rate * db
+
         # Recordint the cost after every 100 iterations
         if i % 100 == 0:
             costs.append(cost)
@@ -230,7 +218,6 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
     
     params = {"w": w,
               "b": b}
-    
     grads = {"dw": dw,
              "db": db}
     
@@ -242,41 +229,40 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
 ###COMPLETE THE FUNCTION
 def predict(w, b, X):
     '''
-    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+    Predict whether the label is 0 or 1 using learned logistic regression
+        parameters (w, b)
     
     Arguments:
-    w -- weights, a numpy array of size (num_px * num_px, 1) in this case (8 * 8, 1)
+    w -- weights, a numpy array of size (num_px * num_px, 1) in this case
+        (8 * 8, 1)
     b -- bias, a scalar 
-    X -- data of size (num_px * num_px, number of examples) in this case (8 * 8, number of examples)
+    X -- data of size (num_px * num_px, number of examples) in this case
+        (8 * 8, number of examples)
     
     Returns:
-    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+    Y_prediction -- a numpy array (vector) containing all predictions (0/1)
+        for the examples in X
     '''
-    
+
     m = X.shape[1]
     Y_prediction = np.zeros((1,m))
-    w = w.reshape(X.shape[0], 1)
-    
-    ## put the code to predict from input data [prediction refers to computing the 'a' variable or sigmoid of w.T*x+b, 
-    ## where w,b is the learned parameters and x is the input vector of the image you are trying to predict]
-    
-    for i in range(A.shape[1]):
-        
-        # Convert probabilities A[0,i] to actual predictions p[0,i], i.e. if probability >=0.5 output 1, else 0
-        Y_prediction = ##complete this line
-        
-    assert(Y_prediction.shape == (1, m))
-    
-    return Y_prediction
 
+    A = np.dot(w, X) + b  
+
+    Y_prediction = np.array(
+        [1 if A[0, i] >= .5 else 0 for i in range(A.shape[1])]
+    ).reshape((1, m))
+
+    assert(Y_prediction.shape == (1, m))
+    return Y_prediction
 
 
 ###COMPLETE THE FUNCTION
 def model(
-    X_train,
-    Y_train,
-    X_test,
-    Y_test,
+    X_train: np.array,
+    Y_train: np.array,
+    X_test: np.array,
+    Y_test: np.array,
     num_iterations=2000,
     learning_rate=0.005,
     print_cost=False
@@ -287,14 +273,17 @@ def model(
     
     Arguments:
     X_train -- training set represented by a numpy array of shape
-            (num_px * num_px, m_train)
+        (num_px * num_px, m_train)
     Y_train -- training labels represented by a numpy array (vector) of
-            shape (1, m_train)
-    X_test -- test set represented by a numpy array of shape 
-            (num_px * num_px, m_test)
-    Y_test -- test labels represented by a numpy array (vector) of shape (1, m_test)
-    num_iterations -- hyperparameter representing the number of iterations to optimize the parameters
-    learning_rate -- hyperparameter representing the learning rate used in the update rule of optimize()
+        shape (1, m_train)
+    X_test -- test set represented by a numpy array of shape
+        (num_px * num_px, m_test)
+    Y_test -- test labels represented by a numpy array (vector) of shape
+        (1, m_test)
+    num_iterations -- hyperparameter representing the number of iterations
+        to optimize the parameters
+    learning_rate -- hyperparameter representing the learning rate used in
+        the update rule of optimize()
     print_cost -- Set to true to print the cost every 100 iterations
     
     Returns:
@@ -304,29 +293,34 @@ def model(
     ### COMPLETE THE CODE BELOW ###
     
     # initialize parameters with zeros 
-    w, b = # call the correct function from above
+    w, b = initialize_with_zeros(X_train.shape[0])
 
     # Gradient descent 
-    parameters, grads, costs = # call the correct function from above
+    parameters, grads, costs = optimize(
+        w=w, 
+        b=b, 
+        X=X_train, 
+        Y=Y_train,
+        num_iterations=num_iterations,
+        learning_rate=learning_rate,
+        print_cost=True
+    )
     
-    # Retrieve parameters w and b from dictionary "parameters" returned by the function above
-    w = # complete this line
-    b = # complete this line
+    # Retrieve parameters w and b from dictionary "parameters" returned
+    #   by the function above
+    w, b = parameters['w'], parameters['b']
     
     # Predict test/train set examples 
-    Y_prediction_test = # call the correct function from above
-    Y_prediction_train = # call the correct function from above
-
-    
+    Y_hat_test = predict(w, b, X_test)
+    Y_hat_train = predict(w, b, X_train)
 
     # Print train/test Errors
-    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
-    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+    print(f"train accuracy: {100 - np.mean(np.abs(Y_hat_train - Y_train)) * 100} %")
+    print(f"test accuracy: {100 - np.mean(np.abs(Y_hat_test - Y_test)) * 100} %")
 
-    
     d = {"costs": costs,
-         "Y_prediction_test": Y_prediction_test, 
-         "Y_prediction_train" : Y_prediction_train, 
+         "Y_hat_test": Y_hat_test, 
+         "Y_hat_train" : Y_hat_train, 
          "w" : w, 
          "b" : b,
          "learning_rate" : learning_rate,
@@ -335,20 +329,27 @@ def model(
     return d
 
 
-
-
-## Calling the function to train the model. FEEL FREE TO MODIFY THE HYPERPARAMETERS (num_iterations AND learning_rate) to experiment with the accuracy
-d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
-
-
-
+## Calling the function to train the model. 
+#  FEEL FREE TO MODIFY THE HYPERPARAMETERS (num_iterations AND learning_rate)
+#  to experiment with the accuracy
+d = model(
+    train_set_x,
+    train_set_y,
+    test_set_x,
+    test_set_y,
+    num_iterations=10000,
+    learning_rate=0.004,
+    print_cost = True
+)
 
 ## Using the model you built to predict the digit of an image
-index = ## put a number between 0 and test_set_size to see the prediction of that image
+index = 1
 plt.imshow(test_set_x[:,index].reshape((num_px, num_px)))
-print ("y = " + str(test_set_y[0,index]) + ", you predicted that it is a \"" + classes[d["Y_prediction_test"][0,index]].decode("utf-8") +  "\" number.")
-
-
+plt.show()
+print (
+    f'y = {str(test_set_y[0,index])}, you predicted that it is a "'
+    f'{classes[d["Y_hat_test"][0,index]]}" number.'
+)
 
 
 ## View how the cost varied with iterations
